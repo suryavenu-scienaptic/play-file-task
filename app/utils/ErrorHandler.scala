aadde20ca448081f09d9ef6e790cba553e33f68f
suryavenu-scienaptic <suryavenu.neelam@scienaptic.com>
2025-03-23 23:20:50 +0530

    Refined ErrorHandling

--- app/utils/ErrorHandler.scala
+++ app/utils/ErrorHandler.scala
@@ -1,12 +1,17 @@
 package utils
 
+import play.api.libs.json._
 import play.api.mvc.Results._
 import play.api.mvc.Result
-import play.api.libs.json.{JsValue, Json}
-import models.{FailureResponse, ValidationErrorResponse, ValidationError}
+import scala.util.control.NonFatal
+import models.FailureResponse
 import java.io.{FileNotFoundException, IOException}
 
-object ErrorHandler {
+class ErrorHandler {
+
+  def failureResponse(message: String): JsValue = {
+    Json.toJson(FailureResponse("failed", message))
+  }
 
   def handleFileErrors: PartialFunction[Throwable, Result] = {
     case e: FileNotFoundException => NotFound(failureResponse(e.getMessage))
@@ -16,11 +21,6 @@ object ErrorHandler {
     case e: IOException if e.getMessage.contains("File path too long") => EntityTooLarge(failureResponse(e.getMessage))
     case e: NumberFormatException => UnprocessableEntity(failureResponse(e.getMessage))
     case e: IOException => InternalServerError(failureResponse(e.getMessage))
-    case e => InternalServerError(failureResponse(e.getMessage))
-  }
-
-  def failureResponse(message: String, errors: List[ValidationError] = List.empty): JsValue = {
-    if (errors.isEmpty) Json.toJson(FailureResponse("failed", message))
-    else Json.toJson(ValidationErrorResponse("failed", message, errors))
+    case NonFatal(e) => InternalServerError(failureResponse(e.getMessage))
   }
 }
